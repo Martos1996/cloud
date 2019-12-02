f6368cdc20bfaa2764eb99f58c8e843329e16b59
Antonio <201401357@alu.comillas.edu>
2019-12-02 17:29:30 +0100

    Yalete

--- src/main/java/asr/proyectoFinal/servlets/Controller.java
+++ src/main/java/asr/proyectoFinal/servlets/Controller.java
@@ -10,6 +10,8 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.nio.Buffer;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -18,19 +20,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.comillas.profile.*;
+
 import asr.proyectoFinal.dao.CloudantPalabraStore;
+import asr.proyectoFinal.services.Analizador;
 import asr.proyectoFinal.services.Traductor;
 import asr.proyectoFinal.dominio.Palabra;
 
 /**
  * Servlet implementation class Controller
  */
-@WebServlet(urlPatterns = {"/listar", "/insertar", "/hablar"})
+@WebServlet(urlPatterns = {"/listar", "/comentarios", "/fotos"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
+		int anger,fear,joy,sadness = 0;
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><head><meta charset=\"UTF-8\"></head><body>");
 		
@@ -45,29 +52,31 @@ public class Controller extends HttpServlet {
 					out.println("Palabras en la BD Cloudant:<br />" + store.getAll());
 				break;
 				
-			case "/insertar":
-				Palabra palabra = new Palabra();
-				String parametro = request.getParameter("palabra");
+			case "/comentarios":
+				String URL = request.getParameter("Env");
+				String token = request.getParameter("token");
+				//out.println(URL);
+				//out.println(token);
+				Profile_model gestor = new Profile_model();
+				try {
+					String[] comentarios = gestor.getComentarios(token,URL);
+					String toAnalizar = new String("");
+					//Traducimos los comentarios para analisis
+					for(int i=0;i<comentarios.length;i++)
+					{
+						toAnalizar = toAnalizar + Traductor.translate(comentarios[i], "es", "en", false) + ".\n";	
+					}
+					ArrayList tonos = Analizador.getTones(toAnalizar);
+					request.setAttribute("tonos", tonos); //categorylist is an arraylist      contains object of class category  
+					getServletConfig().getServletContext().getRequestDispatcher("/displayTones.jsp").forward(request,response);
+					//Analizar estos comentarios
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-				if(parametro==null)
-				{
-				
-					out.println("usage: /insertar?palabra=palabra_a_traducir");
-				}
-				else
-				{
-					if(store.getDB() == null) 
-					{
-						out.println(String.format("Palabra: %s", palabra));
-					}
-					else
-					{
-						parametro = Traductor.translate(parametro,"es","en",false);
-						palabra.setName(parametro);
-						store.persist(palabra);
-					    out.println(String.format("Almacenada la palabra: %s", palabra.getName()));			    	  
-					}
-				}
+
+
 				break;
 		}
 		out.println("</html>");
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,15 +7,71 @@
 <title>Proyecto ASR new...</title>
 </head>
 <body>
-<h1>Ejemplo de Proyecto de ASR con Cloudant ahora con DevOps</h1>
-<hr />
-<p>Opciones sobre la base de datos Cloudant versión 2019:</p>
-<ul>
-<li><a href="listar">Listar</a></li>
-<form action="insertar?palabra.value">
-	<input type="text" name="palabra" value="">
-	<button  value="value" name="name" type=submit >Guardar en base </button> 
-</form> 
-</ul>
+<script>
+
+function statusChangeCallback(response) {
+	 console.log('statusChangeCallback');
+	 console.log(response);
+	 console.log(response.authResponse.accessToken);
+	 //alert(response.authResponse.accessToken);
+	 if (response.status === 'connected') {
+	 window.location.href='Sign_in_Controller.jsp?access_token='+response.authResponse.accessToken; 
+	 } else {
+	 // The person is not logged into your app or we are unable to tell.
+	 document.getElementById('status').innerHTML = 'Please log ' +
+	 'into this app.';
+	 }
+	 }
+
+
+  function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+      statusChangeCallback(response);
+    });
+  }
+
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '680496512439969',
+      cookie     : true,   // Enable cookies to allow the server to access the session.
+	  secret     :'a1d0c005b7bde6b6535ce7850e762b4d',
+      xfbml      : true,                     // Parse social plugins on this webpage.
+      version    : 'v5.0'           // Use this Graph API version for this call.
+    });
+
+
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Returns the login status.
+    });
+  };
+
+  
+  (function(d, s, id) {                      // Load the SDK asynchronously
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+ 
+  function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.access_token);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + ' con un id de ' + response.id + '!';
+    });
+  }
+
+</script>
+
+<fb:login-button scope="public_profile,email,user_photos,user_posts,user_link,user_friends" onlogin="checkLoginState();">
+</fb:login-button>
+<h1>Si, al iniciar sesion le da error recargue esta pagina</h1>
+<div id="status">
+</div>
+
 </body>
 </html>
